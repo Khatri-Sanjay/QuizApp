aa9a178780e57df0696248de5c88b650eccb0b1f
sanjay.khatri <sanjay.khatri@sbsolutionsnepal.com>
2023-04-01 20:12:21 +0545

    first commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String questions[]={
+            "What is the capital city of nepal ?",
+            "What is 1 + 2 ?",
+            "How many legs does a spider have ?",
+            "What is 1 + 2 ?"
+    };
+
+    public static String[][] choices ={
+            {"Kathmandu", "Bhaktapur", "Lalitpur", "Kavre"},
+            {"1", "2", "3", "4"},
+            {"5", "6", "8", "10"},
+            {"1", "2", "3", "4"}
+    };
+
+    public static String correct_answer[]={
+            "Kathmandu",
+            "3",
+            "8",
+            "3"
+    };
+}
